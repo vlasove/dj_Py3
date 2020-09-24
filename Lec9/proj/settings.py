fc31531b6ad5e3b3e5cf1ed0296e59e29fc73c1e
vlasove <evgeny_vlasov@yahoo.com>
2020-09-24 14:17:07 +0300

    added bootstrap

--- Lec9/proj/settings.py
+++ Lec9/proj/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
