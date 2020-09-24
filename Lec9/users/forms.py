50534b722883e5c4d7c39146ca3e3719f69042ed
vlasove <evgeny_vlasov@yahoo.com>
2020-09-24 13:07:38 +0300

    added CustomUser final

--- Lec9/users/forms.py
+++ Lec9/users/forms.py
@@ -8,10 +8,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age', ) 
+        fields = ('username', 'email', 'age' ) 
 
 class CustomUserChangeForm(UserChangeForm):
     
     class Meta(UserChangeForm.Meta):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields 
+        fields = ('username', 'email', 'age' ) 
