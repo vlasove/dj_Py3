f04c84b6b6363fd6771b236e48c0248245fcc796
vlasove <evgeny_vlasov@yahoo.com>
2020-09-24 11:11:33 +0300

    added login/logout/signup

---
+++ Lec8/accounts/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+class SignUpView(CreateView):
+
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
+    form_class = UserCreationForm
--- Lec8/proj/settings.py
+++ Lec8/proj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+######CREDENTIALS FOR LOGIN/LOGOUT
+LOGIN_REDIRECT_URL= 'home'
+LOGOUT_REDIRECT_URL='home'
